c7bfde07b8c0bff0df57553bc649bcabe17ef72f
Artem Goncharov <artem.goncharov@gmail.com>
2023-01-05 09:41:36 +0100

    Update tox.ini && conf.py file
    Performed-by: gitea/infra/otc-metadata/tools/generate_doc_confpy.py

--- umn/source/conf.py
+++ umn/source/conf.py
@@ -24,7 +24,7 @@ extensions = [
 otcdocs_auto_name = False
 otcdocs_auto_version = False
 
-project = 'GaussDB Enterprise-grade Relational Database on the openGauss ecosystem'
+project = 'GaussDB (openGauss)'
 otcdocs_repo_name = 'opentelekomcloud-docs/gaussdb-opengauss'
 # Those variables are required for edit/bug links
 
@@ -81,7 +81,7 @@ html_theme_options = {
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
 
-html_title = "GaussDB Enterprise-grade Relational Database on the openGauss ecosystem - User Guide"
+html_title = "GaussDB (openGauss) - User Guide"
 
 
 # Add any paths that contain custom static files (such as style sheets) here,
@@ -93,6 +93,6 @@ html_static_path = ['_static']
 latex_documents = [
     ('index',
      'opengauss-umn.tex',
-     u'GaussDB Enterprise-grade Relational Database on the openGauss ecosystem - User Guide',
+     u'GaussDB (openGauss) - User Guide',
      u'OpenTelekomCloud', 'manual'),
 ]
