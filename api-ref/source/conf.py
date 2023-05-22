631850754244cdd1c8a002818f55978781f2b40e
SebastianGode <70581801+SebastianGode@users.noreply.github.com>
2023-05-22 15:58:01 +0200

    Update tox.ini && conf.py file (#27)
    Update tox.ini && conf.py file
    
    Performed-by: gitea/infra/otc-metadata/tools/generate_doc_confpy.py
    
    Reviewed-by: Tino Schr

--- api-ref/source/conf.py
+++ api-ref/source/conf.py
@@ -18,7 +18,7 @@ import os
 import sys
 
 extensions = [
-    'otcdocstheme'
+    'otcdocstheme',
 ]
 
 otcdocs_auto_name = False
@@ -88,6 +88,9 @@ html_title = "GaussDB (openGauss) - API Reference"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Do not include sources into the rendered results
+html_copy_source = False
 
 # -- Options for PDF output --------------------------------------------------
 latex_documents = [
